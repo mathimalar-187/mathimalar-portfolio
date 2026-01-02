5fbd5962068cb271ef364acbd18c2dc5bdcafb0a
mathimalar-187 <mathimalar187@gmail.com>
2026-01-02 16:47:12 +0530

    Added new section / updated UI

--- src/components/TopContainer/projectcontainer.jsx
+++ src/components/TopContainer/projectcontainer.jsx
@@ -3,46 +3,54 @@ import { Element } from 'react-scroll'
 import Project1 from './project1'
 import './projectcontainer.css'
 
-const projectcontainer = () => {
+import expense from '../../assets/expense.webp';
+import dashboard from '../../assets/dashboard.png';
+import fruit from '../../assets/fruit.jpg';
+import flight from '../../assets/flight.jpeg';
 
-    const client_projects = [
-{
-   img :"/src/assets/expense.webp",
-   title:"Expense Tracker",
-   desc:"A modern expense tracker platform with advanced filtering, payment integration, and responsive design.",
-   link:"https://github.com/mathimalar-187/finance-trackers.git",
-},
-{
-   img :"/src/assets/dashboard.png",
-   title:"Analystic Dashboard",
-   desc:" A collaborative task management application with real-time updates and team features.",
-   link:"https://github.com/mathimalar-187/analysis-dashboard.git",
-},
-{
-   img :"/src/assets/fruit.jpg",
-   title:"Fruit Shop",
-   desc:" A fresh fruit buying platform for online customers.",
-   link:"https://github.com/mathimalar-187/fruit-shop.git",
-},
-{
-   img :"/src/assets/flight.jpeg",
-   title:"Happy Fly",
-   desc:" Flight booking app.",
-   link:"https://github.com/mathimalar-187/happy-fly.git",
-},
-    ]
+
+const ProjectContainer = () => {
+
+  const client_projects = [
+    {
+      img: expense,
+      title: "Expense Tracker",
+      desc: "A modern expense tracker platform with advanced filtering, payment integration, and responsive design.",
+      link: "https://github.com/mathimalar-187/finance-trackers.git",
+    },
+    {
+      img: dashboard,
+      title: "Analystic Dashboard",
+      desc: "A collaborative task management application with real-time updates and team features.",
+      link: "https://github.com/mathimalar-187/analysis-dashboard.git",
+    },
+    {
+      img: fruit,
+      title: "Fruit Shop",
+      desc: "A fresh fruit buying platform for online customers.",
+      link: "https://github.com/mathimalar-187/fruit-shop.git",
+    },
+    {
+      img: flight,
+      title: "Happy Fly",
+      desc: "Flight booking app.",
+      link: "https://github.com/mathimalar-187/happy-fly.git",
+    },
+  ]
+
   return (
     <Element className='projectcontainer' name='project'>
       <h1>Projects</h1>
-      <p>Here are some projects which i done  making lives of people easy</p>
+      <p>Here are some projects which I have done making lives of people easy</p>
+
       <div className='projectcontainer__project'>
         {client_projects.map((project, index) => (
-          <Project1 
-            key={index} 
-            img={project.img} 
-            title={project.title} 
+          <Project1
+            key={index}
+            img={project.img}
+            title={project.title}
             desc={project.desc}
-            link={project.link} 
+            link={project.link}
           />
         ))}
       </div>
@@ -50,4 +58,4 @@ const projectcontainer = () => {
   )
 }
 
-export default projectcontainer
+export default ProjectContainer
